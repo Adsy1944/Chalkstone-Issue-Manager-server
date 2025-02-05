b98217bf401dd8dd4f4b875d076407f6c05e9365
Adam Crofts <45795045+Adsy1944@users.noreply.github.com>
2025-02-05 16:09:57 +0000

    Completed Issue endpoints with comments for documentation and basic error handling.

--- src/main/java/com/chalkstone/issue_management/service/IssueService.java
+++ src/main/java/com/chalkstone/issue_management/service/IssueService.java
@@ -3,12 +3,9 @@ package com.chalkstone.issue_management.service;
 import com.chalkstone.issue_management.model.Issue;
 import com.chalkstone.issue_management.repository.IssueRepository;
 import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 /**
  * * Service class to carry out any business logic on objects between endpoint and database
@@ -22,7 +19,6 @@ public class IssueService {
      * Constructor and configuration of class
      */
     private final IssueRepository issueRepository;
-    private static final Logger logger = LoggerFactory.getLogger(IssueService.class);
 
     public IssueService(IssueRepository issueRepository) {
         this.issueRepository = issueRepository;
