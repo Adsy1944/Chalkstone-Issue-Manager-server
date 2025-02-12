646bd25e62911f31a395ec912f867507ff479c11
Adam Crofts <45795045+Adsy1944@users.noreply.github.com>
2025-02-12 14:35:18 +0000

    Added employee features

--- src/main/java/com/chalkstone/issue_management/service/EmployeeService.java
+++ src/main/java/com/chalkstone/issue_management/service/EmployeeService.java
@@ -1,11 +1,40 @@
 package com.chalkstone.issue_management.service;
 
+import com.chalkstone.issue_management.model.Employee;
+import com.chalkstone.issue_management.repository.EmployeeRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
+/**
+ * * Service class to carry out any business logic on objects between endpoint and database
+ * */
 @Service
 @Transactional
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.getEmployeeById(id);
+    }
+
+    public ArrayList<Employee> getAllEmployees() {
+        return employeeRepository.getAllEmployees();
+    }
+
+    public int addEmployee(Employee employee) {
+        return employeeRepository.addEmployee(employee.getName(), employee.getRole());
+    }
+
+    public int updateEmployee(Employee employee) {
+        return employeeRepository.updateEmployee(employee.getName(), employee.getRole(), employee.getId());
+    }
 
 
 }
